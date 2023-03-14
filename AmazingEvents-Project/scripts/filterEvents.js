2afaa6da657cc150de1f6e98d5c7b313ec125ab0
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2023-03-14 18:26:24 -0300

    Corregir filtro para eventos futuros y pasados

--- AmazingEvents-Project/scripts/filterEvents.js
+++ AmazingEvents-Project/scripts/filterEvents.js
@@ -6,13 +6,33 @@ form.addEventListener("submit", (e) => {
 const filterEvents = () => {
   let textInput = document.getElementById("input-category").value.toLowerCase()
   let checks = Array.from(document.querySelectorAll("input[type=checkbox]:checked")).map(each => each.value)
-  let filteredEventsArray = eventsData.events.filter(each => {
-    return (
-      (each.name.toLowerCase().includes(textInput) || (each.description.toLowerCase().includes(textInput)))
-      && 
-      (checks.length === 0 || checks.includes(each.category))
-    )
-  })
+
+  let filteredEventsArray = [];
+  if(document.URL.includes("past-events.html")) {
+    filteredEventsArray = pastEventsArray.filter(each => {
+      return (
+        (each.name.toLowerCase().includes(textInput) || (each.description.toLowerCase().includes(textInput)))
+        && 
+        (checks.length === 0 || checks.includes(each.category))
+      )
+    })
+  } else if(document.URL.includes("future-events.html")) {
+    filteredEventsArray = futureEventsArray.filter(each => {
+      return (
+        (each.name.toLowerCase().includes(textInput) || (each.description.toLowerCase().includes(textInput)))
+        && 
+        (checks.length === 0 || checks.includes(each.category))
+      )
+    })
+  } else {
+    filteredEventsArray = eventsData.events.filter(each => {
+      return (
+        (each.name.toLowerCase().includes(textInput) || (each.description.toLowerCase().includes(textInput)))
+        && 
+        (checks.length === 0 || checks.includes(each.category))
+      )
+    })
+  }
 
   if (filteredEventsArray.length > 0) {
     renderEvents("#cards", filteredEventsArray)
